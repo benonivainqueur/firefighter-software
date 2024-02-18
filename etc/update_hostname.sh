53a37dc71681ab1dac1b3beefdbb44d139d42156
benonivainqueur <57690186+benonivainqueur@users.noreply.github.com>
2024-02-18 15:58:50 -0500

    edited update hostname to open etc/hosts

--- etc/update_hostname.sh
+++ etc/update_hostname.sh
@@ -7,15 +7,17 @@ read -p "Enter the new hostname: " new_hostname
 echo "$new_hostname" | sudo tee /etc/hostname > /dev/null
 
 # Update hosts file
-sudo sed -i "s/127.0.1.1.*/127.0.1.1\t$new_hostname/" /etc/hosts
+# sudo sed -i "s/127.0.1.1.*/127.0.1.1\t$new_hostname/" /etc/hosts
 
 # Notify user
 echo "Hostname updated to: $new_hostname"
 
+vi /etc/hosts
+
 # Reboot to apply changes
-read -p "A reboot is required to apply the changes. Would you like to reboot now? (y/n): " choice
-if [[ $choice =~ ^[Yy]$ ]]; then
-    sudo reboot
-else
-    echo "Please reboot your system later to apply the changes."
-fi
+# read -p "A reboot is required to apply the changes. Would you like to reboot now? (y/n): " choice
+# if [[ $choice =~ ^[Yy]$ ]]; then
+#     sudo reboot
+# else
+#     echo "Please reboot your system later to apply the changes."
+# fi
