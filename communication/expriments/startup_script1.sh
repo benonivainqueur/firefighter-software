83e40328f048f155c65a1ae4810e51f07c60a5b0
benonivainqueur <57690186+benonivainqueur@users.noreply.github.com>
2024-03-31 19:25:04 -0400

    added a startup script that will automatically run on the rpi's

---
+++ communication/expriments/startup_script1.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# this script is going to run at startup time. it will wait for the network to be up and running
+# then it will pull the latest code from the git repository. Then it will run another script called start up 2
+cd ~/firefighter-software/communication/experiments
+
+# Function to check Wi-Fi connection status, and keep checking until it is successful
+check_wifi_connection() {
+    if ping -q -c 1 -W 1 google.com >/dev/null; then
+        return 0 # Connection successful
+    else
+        return 1 # Connection failed
+    fi
+}
+
+# Wait for Wi-Fi connection
+while ! check_wifi_connection; do
+    echo "Waiting for Wi-Fi connection..."
+    sleep 1 # Adjust the sleep duration as needed
+done
+
+# Wi-Fi connection detected, proceed with further commands
+
+# Pull the latest code from the git repository
+cd ~/firefighter-software
+git pull
+
+# Run the startup 2 script
+
+# cd ~/firefighter-software/communication/experiments
+# $INTERFACE
+cd ~/firefighter-software/scripts
+./batman.sh
