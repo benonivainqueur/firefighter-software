4faf831b02010edefdac9105b2ce8479495a8da2
benonivainqueur <57690186+benonivainqueur@users.noreply.github.com>
2024-03-31 23:23:09 -0400

    Updated fping.sh script to include hostname and reduce the number of iterations

--- communication/experiments/fping.sh
+++ communication/experiments/fping.sh
@@ -2,13 +2,13 @@
 # Network interface to use
 # $1 is the number of iterations to run fping
 # $2 is the network interface to use
-hostname -I
+
 if [ -z "$1" ]; then
     # fping -c 10 -I bat0 172.27.0.0 172.27.0.1 172.27.0.2 172.27.0.3 172.27.0.4 172.27.0.5 
-    fping -c 10 -I $2 172.27.0.0 172.27.0.1 172.27.0.2 172.27.0.3 172.27.0.4 172.27.0.5 -q
+    hostname -I && fping -c 5 -I $2 172.27.0.0 172.27.0.1 172.27.0.2 172.27.0.3 172.27.0.4 172.27.0.5 -q
 else
 #   echo "Running fping for $1 iterations"
-    fping -c $1 -I $2 172.27.0.0 172.27.0.1 172.27.0.2 172.27.0.3 172.27.0.4 172.27.0.5 -q
+    hostname -I && fping -c $1 -I $2 172.27.0.0 172.27.0.1 172.27.0.2 172.27.0.3 172.27.0.4 172.27.0.5 -q
 fi
 
 
